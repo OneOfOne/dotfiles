6b70ce8c054aad718166ce45edf5f0eecd553b30
OneOfOne <oneofone@gmail.com>
2018-05-07 06:23:56 +0200

    arch aliases

---
+++ .env.d/04-arch.zsh
@@ -0,0 +1,11 @@
+alias y="env PATH=/usr/bin yaourt"
+alias yup="y -Syu --aur"
+alias yr="y -Rscd"
+alias yi="y -S"
+alias ys="y -Ss"
+
+alias am="env PATH=/usr/bin aurman"
+alias amup="am -Syu"
+alias amr="am -Rscd"
+alias ami="am -S"
+alias ams="am -Ss"
