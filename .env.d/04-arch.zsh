2a4f274f8c1b3b18712a1bba26c07b8814f0b749
OneOfOne <oneofone@gmail.com>
2018-12-15 01:52:42 +0200

    move to yay and update go settings

--- .env.d/04-arch.zsh
+++ .env.d/04-arch.zsh
@@ -1,13 +1,4 @@
-
-
-alias am="sudo -u aur env -i PATH=/usr/bin:/usr/bin/core_perl HOME=/tmp/aurman LANG=en_US.UTF-8 aurman"
-alias amup="am -Syu"
-alias amr="am -Rscd"
-alias ami="am -S"
-alias ams="am -Ss"
-
-alias y="am"
-alias yup="y -Syu"
+alias y="yay"
 alias yr="y -Rscd"
 alias yi="y -S"
 alias ys="y -Ss"
