fdedec2040c77090852fb140e558a1cdb4f27b3f
OneOfOne <oneofone@gmail.com>
2023-08-23 22:47:38 -0500

    auto cd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+local dir = ''
+for i = 1, 4 do
+	if vim.fn.isdirectory(vim.v.argv[i]) ~= 0 then
+		dir = vim.v.argv[i]
+		break
+	end
+end
+
+if dir ~= '' then
+	vim.cmd('cd ' .. dir)
+end
