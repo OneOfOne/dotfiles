af6393be22d6d0be77b3c6727169fb8b4251da5d
OneOfOne <oneofone@gmail.com>
2023-08-23 22:08:03 -0500

    move to lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,3 @@
-require("plugins")
--- require('plugins/treesetter')
-require("plugins/telescope")
-require("plugins/dap")
-require("plugins/nvimtree")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
-require("settings")
-require("settings/ui")
-require("settings/git")
-
--- require('lsp')
--- require('lsp/gopls')
-
-require("keymap")
-
-function _G.wait_for_plugins()
-	local dir = require("utils").real_cwd()
-	if dir then vim.cmd("cd " .. dir) end
-end
-
--- run after all plugins are loaded / unloaded
-vim.cmd([[ autocmd VimEnter * lua wait_for_plugins() ]])
--- .config/nvim/lua/plugins/treesetter.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-treesitter.configs').setup({
-	ensure_installed = "all",
-	highlight = {
-		enable = true,
-	},
-	context_commentstring = {
-		enable = true
-	}
-})
