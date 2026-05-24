077039ea9a30d0a8ecf6c8d1d83b1e5a2b740716
OneOfOne <oneofone@gmail.com>
2026-05-24 11:51:22 -0500

    updates

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -26,7 +26,7 @@ require('lazy').setup({
 		{ import = 'lazyvim.plugins.extras.lang.yaml' },
 		{ import = 'lazyvim.plugins.extras.lang.toml' },
 		{ import = 'lazyvim.plugins.extras.lang.markdown' },
-		{ import = 'lazyvim.plugins.extras.lang.terraform' },
+		-- { import = 'lazyvim.plugins.extras.lang.terraform' },
 
 		{ import = 'lazyvim.plugins.extras.lang.typescript' },
 		{ import = 'lazyvim.plugins.extras.lang.python' },
